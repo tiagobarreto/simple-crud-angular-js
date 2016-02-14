a6cb0a5e80a9b9cc70e1e2b79af2c510f91c32c3
Tiago Barreto <falecom@tiagobarreto.com>
2016-02-14 16:18:42 -0200

    Fixed toast messages.

--- app/controllers.js
+++ app/controllers.js
@@ -10,6 +10,7 @@ function ListaComprasController($scope) {
     $scope.adicionaItem = function () {
         $scope.itens.push({produto: $scope.item.produto, quantidade: $scope.item.quantidade, comprado: false});
         $scope.item.produto = $scope.item.quantidade = '';
+        toastr.success("Item adicionado com sucesso.");
     };
 
     $scope.editarItem = function(index){
@@ -20,9 +21,11 @@ function ListaComprasController($scope) {
     $scope.applyChanges = function(index){
         $scope.item = {};
         $scope.edit = false;
+        toastr.success("Item alterado com sucesso.");
     };
 
     $scope.deleteItem = function(index){
         $scope.itens.splice(index, 1);
+        toastr.success("Item removido com sucesso.");
     };
 }
